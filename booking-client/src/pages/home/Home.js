fb7012b64c8634c459ba86948b79cc73c0444401
unknown <rakibulanas033@gmail.com>
2022-08-05 10:13:05 +0600

    add client site

---
+++ booking-client/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Navbar from "../../Components/navbar/Navbar";
+import "./home.css";
+const Home = () => {
+	return (
+		<div>
+			<Navbar />
+			<Header />
+		</div>
+	);
+};
+
+export default Home;

da2b9ef069a79e2a5c019241c61c0f5d489f4929
unknown <rakibulanas033@gmail.com>
2022-08-06 22:13:51 +0600

    add search

--- booking-client/src/Components/Header/Header.js
+++ booking-client/src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import {
 	faBed,
+	faCalendarDay,
 	faCar,
 	faPlane,
 	faTaxi,
@@ -12,27 +13,50 @@ const Header = () => {
 		<div className="header">
 			<div className="headerContainer">
 				<div className="headerList">
-					<div className="headerList">
-						<div className="headerListItem active">
-							<FontAwesomeIcon icon={faBed} />
-							<span>Stays</span>
-						</div>
-						<div className="headerListItem">
-							<FontAwesomeIcon icon={faPlane} />
-							<span>Flights</span>
-						</div>
-						<div className="headerListItem">
-							<FontAwesomeIcon icon={faCar} />
-							<span>Car rentals</span>
-						</div>
-						<div className="headerListItem">
-							<FontAwesomeIcon icon={faBed} />
-							<span>Attractions</span>
-						</div>
-						<div className="headerListItem">
-							<FontAwesomeIcon icon={faTaxi} />
-							<span>Airport taxis</span>
-						</div>
+					<div className="headerListItem active">
+						<FontAwesomeIcon icon={faBed} />
+						<span>Stays</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faPlane} />
+						<span>Flights</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faCar} />
+						<span>Car rentals</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faBed} />
+						<span>Attractions</span>
+					</div>
+					<div className="headerListItem">
+						<FontAwesomeIcon icon={faTaxi} />
+						<span>Airport taxis</span>
+					</div>
+				</div>
+				<h1 className="headerTitle">Find your next stay</h1>
+				<p className="headerDesc">
+					Search deals on hotels, homes, and much more...
+				</p>
+				<button className="headerBtn">Sign in/ Register</button>
+
+				{/* header search  */}
+				<div className="headerSearch">
+					<div className="headerSearchItem">
+						<FontAwesomeIcon icon={faBed} className="headerIcon" />
+						<input
+							type="text"
+							placeholder="Where are you going?"
+							className="headerSearchInput"
+						/>
+					</div>
+					<div className="headerSearchItem">
+						<FontAwesomeIcon icon={faCalendarDay} className="headerIcon" />
+						<span className="headerSearchText">date to date</span>
+					</div>
+					<div className="headerSearchItem">
+						<FontAwesomeIcon icon={faBed} className="headerIcon" />
+						<span className="headerSearchText">2 adults 2 children 1 room</span>
 					</div>
 				</div>
 			</div>
